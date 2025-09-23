d452a5a2d040c1bd4668cee5882d0ad959c8073e
Diego Díaz Auffray <diegodiazauffray@gmail.com>
2025-09-23 22:53:33 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,12 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: It is strongly recommended to move this configuration to environment variables
 // for security and flexibility. This is a temporary measure for debugging.
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_AUTH_DOMAIN_HERE",
-  projectId: "YOUR_PROJECT_ID_HERE",
-  storageBucket: "YOUR_STORAGE_BUCKET_HERE",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
-  appId: "YOUR_APP_ID_HERE",
-  measurementId: "YOUR_MEASUREMENT_ID_HERE"
+  "projectId": "studio-6718836827-4de5a",
+  "appId": "1:577171585378:web:740c8bbd265d2f36f0143b",
+  "apiKey": "AIzaSyAFMYISF-g2onEeORtvwVxaoONyiHJupl0",
+  "authDomain": "studio-6718836827-4de5a.firebaseapp.com",
+  "measurementId": "G-ZP3WVY5QMQ",
+  "messagingSenderId": "577171585378"
 };
 
 

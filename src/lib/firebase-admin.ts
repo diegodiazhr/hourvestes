b5937758951a58b67b6e0179dde70a7a5f1b182c
Diego Díaz Auffray <diegodiazauffray@gmail.com>
2025-09-24 19:52:25 +0000

    Try fixing this error: `Runtime Error: Error: Firebase Admin SDK configu

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -6,7 +6,7 @@ import { getStorage, Storage } from 'firebase-admin/storage';
 import dotenv from 'dotenv';
 import path from 'path';
 
-// This will load the .env file from the root of the project, NOT from the functions directory
+// This will load the .env file from the root of the project.
 dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 
 
